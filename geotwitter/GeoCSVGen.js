517469a37d92c7a0ed8a0e8bc16539656f513055
zhangwei217245 <zhangwei217245@gmail.com>
2016-03-24 21:09:32 -0500

    update, finally solved the JS callback issue. Need to increase the old gen mem space
    --max_old_space_size=2000000
    
    --max_new_space_size

---
+++ geotwitter/GeoCSVGen.js
@@ -0,0 +1,23 @@
+/**
+ * Created by zhangwei on 3/24/16.
+ */
+const config = require('node-yaml-config');
+var conf = config.load('./config/geotwitter.yaml', 'default');
+const scale = require('./scale/scale').scale(500);
+const redis = require('redis').createClient(conf.database);
+const S=require('string');
+
+var size = scale.size();
+
+scale.walkGrids(function (x, y) {
+    (function (i,j) {
+        var key = i+','+j;
+        redis.scard(key, function (err, data) {
+            if (j==0) {
+                console.log("\n")
+            }
+            console.log(data+",");
+
+        })
+    })(x,y)
+})
