5ce837ce1ca41171622874c2537932acca3e1661
Wei Zhang <X-Spirit.zhang@ttu.edu>
2016-07-25 14:49:01 -0500

    user sentiment

--- mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/twitteruser/UserSentiment.scala
+++ mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/twitteruser/UserSentiment.scala
@@ -1,22 +1,20 @@
 package x.spirit.dynamicjob.mockingjay.twitteruser
 
 import org.apache.hadoop.hbase.client.Scan
-import org.apache.hadoop.hbase.filter.{PageFilter, PrefixFilter}
+import org.apache.hadoop.hbase.filter.PageFilter
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
-import x.spirit.dynamicjob.mockingjay.corenlp.functions._
-import x.spirit.dynamicjob.mockingjay.hbase._
-import x.spirit.dynamicjob.mockingjay.hbase.HBaseConfig
+import org.json.JSONArray
+import x.spirit.dynamicjob.mockingjay.hbase.{HBaseConfig, _}
+import x.spirit.dynamicjob.twitteruser.PoliticalPreference
 
 /**
   * Created by zhangwei on 7/7/16.
   */
-object UserSentiment extends App{
+object UserSentiment extends App {
 
   lazy val sparkConf = new SparkConf().setAppName("UserSentimentCalculator")
   lazy val sc = new SparkContext(sparkConf)
-  lazy val sqlContext = new SQLContext(sc)
 
   implicit val config = HBaseConfig(
     hbaseXmlConfigFile = "hbase-site.xml"
@@ -28,15 +26,41 @@ object UserSentiment extends App{
       * https://hbase.apache.org/apidocs/org/apache/hadoop/hbase/filter/package-summary.html
       * Here, it's better to use PageFilter and
       */
-    val scan = new Scan(Bytes.toBytes("0"), new PageFilter(100l))
-
-    sc.hbase[String]("sent_blue_red_2012", Set("tsent"), scan)
-      .map({ case (k, v) =>
-        val tsent = v("tsent")
-        tsent.map({case(tid, jsonArr)=>
-          System.out.println(jsonArr);
-        })
-      })
-
+    var notfinished = true;
+    while (notfinished) {
+      val scan = new Scan(Bytes.toBytes("0"), new PageFilter(100l))
+      val scanRst = sc.hbase[String]("sent_blue_red_2012", Set("tsent"), scan)
+      if (scanRst.count() > 0) {
+        scanRst.map({ case (k, v) =>
+          val uid = k;
+          val tsent = v("tsent")
+          val blue_red = tsent.map({ case (tid, jsonBytes) =>
+            val jsonArr = new JSONArray(Bytes.toString(jsonBytes));
+            val blueScore = jsonArr.getInt(1)
+            val redScore = jsonArr.getInt(2)
+            (blueScore, redScore, 1)
+          })
+          val sumBlue = blue_red.map(_._1).sum
+          val sumRed = blue_red.map(_._2).sum
+          var decision = PoliticalPreference.None;
+          if (sumBlue.equals(sumRed)) {
+            decision = PoliticalPreference.Neutral;
+          } else if (sumBlue > sumRed) {
+            decision = PoliticalPreference.Blue;
+          } else {
+            decision = PoliticalPreference.Red;
+          }
+          uid -> Map({
+            "political" -> Map(
+              "type" -> Bytes.toBytes(decision.getValue),
+              "sumblue" -> Bytes.toBytes(sumBlue),
+              "sumred" -> Bytes.toBytes(sumRed)
+            )
+          })
+        }).toHBase("machineLearn2012")
+      } else {
+        notfinished = false;
+      }
+    }
   }
 }
