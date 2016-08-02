863987dab6c93e5fa77bc025c0a489697d85a61c
Wei Zhang <X-Spirit.zhang@ttu.edu>
2016-08-01 23:11:29 -0500

    Two scanner

---
+++ mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/scanner/LocationScanner.scala
@@ -0,0 +1,54 @@
+package x.spirit.dynamicjob.mockingjay.scanner
+
+import org.apache.hadoop.hbase.filter.PrefixFilter
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.{SparkConf, SparkContext}
+import org.json.JSONArray
+import x.spirit.dynamicjob.mockingjay.hbase.{HBaseConfig, _}
+import x.spirit.dynamicjob.mockingjay.twitteruser.PoliticalPreference._
+
+import scala.collection.mutable
+
+/**
+  * Created by zhangwei on 8/1/16.
+  */
+object LocationScanner extends App{
+
+  override def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("UserSentimentCalculator")
+    val sc = new SparkContext(sparkConf)
+
+    implicit val config = HBaseConfig(
+      hbaseXmlConfigFile = "hbase-site.xml"
+    )
+
+    /**
+      * For all hbase filters, refer to :
+      * https://hbase.apache.org/apidocs/org/apache/hadoop/hbase/filter/package-summary.html
+      * Here, it's better to use PageFilter and
+      */
+    var startRowPrefix = 1;
+    var allRst:Array[(String, Int)] = Array();
+    while (startRowPrefix <= 9) {
+      System.out.println("Start row prefix = %d".format(startRowPrefix))
+      val scanRst = sc.hbase[String]("twitterUser", Set("tweet"),
+        new PrefixFilter(Bytes.toBytes(startRowPrefix.toString)))
+      val typeMap = scanRst.map({ case (k, v) =>
+        val uid = k;
+        val tweet = v("tweet")
+        val types = tweet.map({ case (tid, jsonBytes) =>
+          val jsonArr = new JSONArray(Bytes.toString(jsonBytes));
+          val content = jsonArr.getJSONArray(1);
+          val placeType = content.getString(2);
+          (placeType, 1)
+        }).groupBy(_._1).map({case(k,lst) =>
+          (k, lst.size)
+        })
+        types
+      }).flatMap({map=>map}).groupBy(_._1).map({case(k,v)=>(k,v.map(_._2).sum)}).collect
+      allRst = allRst.union(typeMap);
+      startRowPrefix += 1;
+    }
+    System.out.println(allRst.groupBy(_._1).map({case(k,v)=>(k,v.map(_._2).sum)}).collect)
+  }
+}
---
+++ mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/scanner/TweetsScanner.scala
@@ -0,0 +1,85 @@
+package x.spirit.dynamicjob.mockingjay.scanner
+
+import org.apache.hadoop.hbase.filter.PrefixFilter
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.{SparkConf, SparkContext}
+import org.json.JSONArray
+import x.spirit.dynamicjob.mockingjay.hbase.{HBaseConfig, _}
+import x.spirit.dynamicjob.mockingjay.twitteruser.PoliticalPreference._
+
+/**
+  * Created by zhangwei on 8/1/16.
+  */
+object TweetsScanner extends App {
+
+  override def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("UserSentimentCalculator")
+    val sc = new SparkContext(sparkConf)
+
+    implicit val config = HBaseConfig(
+      hbaseXmlConfigFile = "hbase-site.xml"
+    )
+
+    /**
+      * For all hbase filters, refer to :
+      * https://hbase.apache.org/apidocs/org/apache/hadoop/hbase/filter/package-summary.html
+      * Here, it's better to use PageFilter and
+      */
+    var startRowPrefix = 1;
+    var overallTweet = 0.0D;
+    var overallBlueTweet = 0.0D;
+    var overallRedTweet = 0.0D;
+    var overallUser = 0L;
+    var blueUser = 0L;
+    var redUser = 0L;
+    var neutralUser = 0L;
+    while (startRowPrefix <= 9) {
+      System.out.println("Start row prefix = %d".format(startRowPrefix))
+      val scanRst = sc.hbase[String]("sent_blue_red_2012", Set("tsent"),
+        new PrefixFilter(Bytes.toBytes(startRowPrefix.toString)));
+
+      val intermediateResult = scanRst.map({ case (k, v) =>
+        val uid = k;
+        val tsent = v("tsent")
+        val blue_red = tsent.map({ case (tid, jsonBytes) =>
+          val jsonArr = new JSONArray(Bytes.toString(jsonBytes));
+          val blueScore = jsonArr.getInt(1)
+          val redScore = jsonArr.getInt(2)
+          (blueScore, redScore, 1)
+        })
+
+        val tweetCount = blue_red.map(_._3).sum
+        val blueTweetCount = blue_red.filter(r => {r._1 > r._2}).map(_._3).sum
+        val redTweetCount = blue_red.filter(r => {r._2 > r._1}).map(_._3).sum
+
+        val sumBlue = blue_red.map(_._1).sum
+        val sumRed = blue_red.map(_._2).sum
+        var decision = None;
+        if (sumBlue.equals(sumRed)) {
+          decision = Neutral;
+        } else if (sumBlue > sumRed) {
+          decision = Blue;
+        } else {
+          decision = Red;
+        }
+
+        (tweetCount, blueTweetCount, redTweetCount, decision.id)
+
+      })
+      overallTweet+=intermediateResult.map(_._1).sum()
+      overallBlueTweet+=intermediateResult.map(_._2).sum()
+      overallRedTweet+=intermediateResult.map(_._3).sum()
+
+      overallUser+=intermediateResult.count;
+      blueUser+=intermediateResult.filter(_._4 == Blue.id).count;
+      redUser+=intermediateResult.filter(_._4 == Red.id).count;
+      neutralUser+=intermediateResult.filter(_._4 == Neutral.id).count;
+
+      startRowPrefix += 1;
+    }
+    System.out.println("[FINAL TWEET RST] Overall %d, Blue %d, Red %d"
+      .format(overallTweet,overallBlueTweet,overallRedTweet));
+    System.out.println("[FINAL USER RST] Overall %d, Blue %d, Red %d, Neutral %d"
+      .format(overallUser, blueUser, redUser, neutralUser))
+  }
+}
