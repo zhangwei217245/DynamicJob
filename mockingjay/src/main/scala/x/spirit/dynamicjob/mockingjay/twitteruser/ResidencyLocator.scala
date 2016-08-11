86371dba007584f79739f98aac8c116106fde557
Wei Zhang <X-Spirit.zhang@ttu.edu>
2016-08-10 23:07:16 -0500

    count location type

--- mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/twitteruser/ResidencyLocator.scala
+++ mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/twitteruser/ResidencyLocator.scala
@@ -1,36 +1,32 @@
-package x.spirit.dynamicjob.mockingjay.scanner
+package x.spirit.dynamicjob.mockingjay.twitteruser
 
 import org.apache.hadoop.hbase.filter.PrefixFilter
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.{SparkConf, SparkContext}
 import org.json.JSONArray
 import x.spirit.dynamicjob.mockingjay.hbase.{HBaseConfig, _}
-import x.spirit.dynamicjob.mockingjay.twitteruser.PoliticalPreference._
-
-import scala.collection.mutable
 
 /**
-  * Created by zhangwei on 8/1/16.
+  * Created by zhangwei on 8/10/16.
   */
-object LocationScanner extends App{
+object ResidencyLocator extends App {
 
   override def main(args: Array[String]) {
-    val sparkConf = new SparkConf().setAppName(this.getClass.getName)
+    val sparkConf = new SparkConf().setAppName("ResidencyLocator")
     val sc = new SparkContext(sparkConf)
 
     implicit val config = HBaseConfig(
       hbaseXmlConfigFile = "hbase-site.xml"
     )
-    config.get.set("hbase.rpc.timeout", "1800000");
 
     /**
       * For all hbase filters, refer to :
       * https://hbase.apache.org/apidocs/org/apache/hadoop/hbase/filter/package-summary.html
       * Here, it's better to use PageFilter and
       */
-    var startRowPrefix = 10;
-    var allRst:Array[(String, Int)] = Array();
-    while (startRowPrefix <= 99) {
+    var startRowPrefix = 1;
+    var allRst: Array[(String, Int)] = Array();
+    while (startRowPrefix <= 9) {
       System.out.println("Start row prefix = %d".format(startRowPrefix))
       val scanRst = sc.hbase[String]("twitterUser", Set("tweet"),
         new PrefixFilter(Bytes.toBytes(startRowPrefix.toString)))
@@ -39,18 +35,23 @@ object LocationScanner extends App{
         val tweet = v("tweet")
         val types = tweet.map({ case (tid, jsonBytes) =>
           val jsonArr = new JSONArray(Bytes.toString(jsonBytes));
+
+          val time = jsonArr.getJSONArray(0);
           val content = jsonArr.getJSONArray(1);
+          val coord = jsonArr.getJSONArray(2);
+
+
           val placeType = content.getString(2);
           (placeType, 1)
-        }).groupBy(_._1).map({case(k,lst) =>
+        }).groupBy(_._1).map({ case (k, lst) =>
           (k, lst.size)
         })
         types
-      }).flatMap({map=>map}).groupBy(_._1).map({case(k,v)=>(k,v.map(_._2).sum)}).collect
+      }).flatMap({ map => map }).groupBy(_._1).map({ case (k, v) => (k, v.map(_._2).sum) }).collect
       allRst = Array.concat(allRst, typeMap);
       startRowPrefix += 1;
     }
-    allRst.groupBy(_._1).map({case(k,v)=>(k,v.map(_._2).sum)}).foreach({case(k,v)=>
+    allRst.groupBy(_._1).map({ case (k, v) => (k, v.map(_._2).sum) }).foreach({ case (k, v) =>
       System.out.println("%s -> %s ".format(k, v))
     })
   }
