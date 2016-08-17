acc7d57b96aa58c270f4cfa750be3fb0d5661779
Wei Zhang <X-Spirit.zhang@ttu.edu>
2016-08-17 15:06:03 -0500

    performance tuning.

--- mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/twitteruser/ResidencyLocator.scala
+++ mockingjay/src/main/scala/x/spirit/dynamicjob/mockingjay/twitteruser/ResidencyLocator.scala
@@ -46,9 +46,9 @@ object ResidencyLocator extends App {
       * https://hbase.apache.org/apidocs/org/apache/hadoop/hbase/filter/package-summary.html
       * Here, it's better to use PageFilter and
       */
-    var startRowPrefix = 1000
+    var startRowPrefix = 10000
     var allRst: Array[(String, Int)] = Array()
-    while (startRowPrefix <= 9999) {
+    while (startRowPrefix <= 99999) {
       println("Start row prefix = %d".format(startRowPrefix))
       val scan = new Scan()
       scan.setCaching(100)
