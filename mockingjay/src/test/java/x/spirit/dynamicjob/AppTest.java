a6efd857a03826f489ac089f0d520975990aa597
Wei Zhang <X-Spirit.zhang@ttu.edu>
2017-04-21 16:52:39 -0500

    adding versioned file

---
+++ mockingjay/src/test/java/x/spirit/dynamicjob/AppTest.java
@@ -0,0 +1,42 @@
+package x.spirit.dynamicjob;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        String a = "s829,s_s w\\s/w-w";
+        for (String x : a.split("\\W")){
+            System.out.println(x);
+        }
+        assertTrue( true );
+    }
+}
